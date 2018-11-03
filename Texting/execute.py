a2f212ab8e7aa5c50aa6c76879dbf1ead9ae9d36
and1zhao <awzbball@gmail.com>
2018-11-03 16:41:52 -0700

    testing split

--- Texting/execute.py
+++ Texting/execute.py
@@ -7,14 +7,20 @@ data = eval(line1)
 text = data["text"][0]
 command = text.split(" ")
 
-"""if text[:5] == 'Split':
-    print("Splitting " + text[6:])
-    total = float(text[6:])
-    print(total/4)"""
+if command[0] == 'Split':
+    value = command[1]
+    users = command[2:]
+    for user in users:
+        str = 'venmo charge ' + str(user) + ' ' + str(round(amount/(len(users)-1), 2)) + ' nb' #switch to manual message later
+        print(str)
+        os.system(str)
+    # print("Splitting " + text[6:])
+    # total = float(text[6:])
+    # print(total/4)
 
 if command[0] == 'Request':
-    value = command[1]
-    number = command[2]
-    str = "venmo charge " + str(number) + ' ' + str(value) + " thanks"
+    amount = command[1]
+    user = command[2]
+    str = 'venmo charge ' + str(user) + ' ' + str(amount) + ' thanks'
     print(str)
     os.system(str)
