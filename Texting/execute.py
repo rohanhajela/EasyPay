066f1fae07e0343c343985fcd9189e714dd8e470
rbatra2000 <ritik.batra2000@gmail.com>
2018-11-04 04:01:33 -0800

    final commit with all functionality

--- Texting/execute.py
+++ Texting/execute.py
@@ -2,6 +2,8 @@ import os
 #import sendsms
 import subprocess
 import venmopush
+import sendsms
+import time
 file = open('response.json','r')
 
 line1 = file.readline()
@@ -14,14 +16,16 @@ sender = data["msisdn"][0]
 command = text.split(" ")
 
 all_functions = ['#charge', '#split', '#billpay']
-function, amount, users, message_array, message = '', '', [], [], ''
+function, amount, users, message_array, reason = '', '', [], [], ''
 
 new_data = []
-venmo_database = {"Andy": 15103585981}
+venmo_database = {"andy": 15103585981, "rohan": 14085042986, "ritik": 14088939075, "sahil": 15106485141}
+
+possible_functions = {'#charge': ['owe', 'pay', 'send', 'venmo', 'give', 'spot', 'request'], '#split': ['divide', 'split'], '#billpay': []}
 
 
 def parse_NLU():
-    global new_data
+    global new_data, function, users, amount, reason
     #Change 1st Line
     parameter = open('parameters.json', 'r')
     cur = parameter.readlines()
@@ -42,25 +46,71 @@ def parse_NLU():
     "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-03-19"'
     output = subprocess.check_output(command, shell=True)
     output = eval(output)
-    user = output["entities"][0]["text"]
-    user = venmo_database.get(user, 0)
-    function = output["entities"][1]["text"]
-    if function == "owe":
-        function = "#charge"
-    elif function == "pay":
-        function = "#charge"
-    amount = output["entities"][2]["text"]
+    print("Output: " + str(output))
+    #user = output["entities"][0]["text"]
+
+    for element in output.get("entities", {'type': ''}):
+        if element["type"] == "Name":
+            users.append(element["text"])
+        elif element["type"] == "Function":
+            function = element["text"]
+        elif element["type"] == "Amount":
+            amount = element["text"]
+        elif element["type"] == "Reason":
+            reason = element["text"]
+
+
+    #user = output.get("entities", -1)
+
+    for i in range(len(users)):
+        users[i] = users[i].lower()
+        users[i] = venmo_database.get(users[i], 0)
+    #function = output["entities"][1]["text"]
+    #function = output.get("entities", -1)
+
+    for fn in possible_functions:
+        for possible_fn in possible_functions[fn]:
+            if function == possible_fn:
+                function = fn
+
+    # if function == "owe":
+    #     function = "#charge"
+    # elif function == "pay":
+    #     function = "#charge"
+    # elif function == "send":
+    #     function = "#charge"
+    #amount = output["entities"][2]["text"]
+
+    if not amount or not function or not users:
+        return sendAll(sender,text)
+
     if amount[:1] == "$":
         amount = amount[1:]
-    reason = output["entities"][3]["text"]
+    #reason = output["entities"][3]["text"]
+    #print("Reason: " + str(reason))
 
-    new_data = [function, amount, user, reason]
+    if not reason:
+        reason = "default"
+
+    new_data = [function, amount, users, reason]
+
+def sendAll(sender,message):
+    sndr_name = ''
+    for name in venmo_database:
+        if venmo_database[name] == int(sender):
+            sndr_name = name
+    for name in venmo_database:
+        if venmo_database[name] != int(sender):
+            msg = sndr_name.capitalize() + ': ' + message
+            sendsms.send(venmo_database[name], msg)
+            time.sleep(0.5)
+
 
 parse_NLU()
-#print(new_data)
+print("Parsed: " + str(new_data))
 
 # parses through code, splits into the function, amount, users, and an optional message
-def parse(command_array):
+"""def parse(command_array):
     global function, amount, message
     if command_array[0] in all_functions: #see if fn is not one of our pre-defined functions
         function = command_array[0]
@@ -78,10 +128,13 @@ def parse(command_array):
     for message_part in message_array:
         message += message_part + ' '
     if message == '':
-        message = 'default'
+        message = 'default'"""
 
-parse(new_data)
+#parse(new_data)
 #print(message)
-venmopush.maketransaction(function, amount, users, message, sender)
+command = [function, amount, users, reason, sender]
+
+print("Executing:" + str(command))
+venmopush.maketransaction(function, amount, users, reason, sender)
 
 file.close()
--- Texting/sendsms.py
+++ Texting/sendsms.py
@@ -3,6 +3,7 @@ import nexmo
 client = nexmo.Client(key='6b2a6370', secret='mfN15VZwqx00mDUy')
 
 def send(number, message):
+    print(number)
     client.send_message({
         'from': '14085029847',
         'to': number,
