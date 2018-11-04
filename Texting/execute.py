df6c1201290878fe27cdc1401018753f260e97c1
rbatra2000 <ritik.batra2000@gmail.com>
2018-11-03 17:01:24 -0700

    added response to text

--- Texting/execute.py
+++ Texting/execute.py
@@ -1,10 +1,13 @@
 import os
+import sendsms
+import subprocess
 file = open('response.json','r')
 
 line1 = file.readline()
 data = eval(line1)
 
 text = data["text"][0]
+sender = data["msisdn"][0]
 command = text.split(" ")
 
 """if text[:5] == 'Split':
@@ -16,5 +19,8 @@ if command[0] == 'Request':
     value = command[1]
     number = command[2]
     str = "venmo charge " + str(number) + ' ' + str(value) + " thanks"
-    print(str)
-    os.system(str)
+    output = subprocess.check_output(str, shell=True)
+    print(output)
+    sendsms.send(sender, output)
+
+file.close()
